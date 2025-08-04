b0d4e5bce767d29aa590a59e5af67a4a0042925c
Fernando <fernando403@gmail.com>
2025-08-03 23:59:51 -0700

    bid feature fixed 2

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Inject Firebase token
